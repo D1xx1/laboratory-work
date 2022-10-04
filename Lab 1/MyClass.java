8d7c9e781bd71c4c08fe3156a2d5a51a6787a5a6
Viktor <95516464+D1xx1@users.noreply.github.com>
2022-10-04 19:21:49 +0700

    Added find method.

--- Lab 1/MyClass.java
+++ Lab 1/MyClass.java
@@ -7,36 +7,59 @@ public class MyClass {
 
     public static int[] input(){
 
-        Scanner innum = new Scanner(System.in);
-        System.out.print("Введите числитель: ");
-        int num = innum.nextInt();
-
         Scanner in = new Scanner(System.in);
+        System.out.print("Введите числитель: ");
+        int num = in.nextInt();
+
         System.out.print("Введите знаменатель: ");
         int denum = in.nextInt();
-        innum.close();
         in.close();
         return new int[] {num, denum};
     }
 
-    public static void write (String filename, int[]x) throws IOException{
+    public static int[] find() {
+        int integer = 0;
+        int[] x = input();
+        int num = x[0];
+        int denum = x[1];
+
+        if (num > denum) {
+            while (num >= denum) {
+                num = num - denum;
+                integer = integer +1;
+                if (num == denum){
+                    num = 0;
+                    denum = 0;
+                    integer = integer +1;
+                    break;
+                }
+            }
+        }
+
+        else if (num == denum){
+            integer = 1;
+            num = 0;
+            denum = 0;
+        }
+
+        return new int[] {integer, num, denum,x[0],x[1]};
+        
+    }
+
+    public static void write (String filename, int[]x) throws IOException {
 
         BufferedWriter outputWriter = null;
         outputWriter = new BufferedWriter(new FileWriter("nums.txt",true));
-        outputWriter.write("Числитель: "+x[0]+", знаменатель: "+x[1]+"\n");
+        outputWriter.write("Целое число: "+x[0]+", числитель: "+x[1]+", знаменатель: "+x[2]+". Исходные данные: "+x[3]+", "+x[4]+".\n");
         outputWriter.flush();
         outputWriter.close();
+        System.out.println("Целое число: "+x[0]+", числитель: "+x[1]+", знаменатель: "+x[2]+". Исходные данные: "+x[3]+", "+x[4]+".");
     }
-
-    public static void finding_a_fraction(){
-
-    }
-
-
+    
     public static void main(String[] args) throws IOException {
-        int[] x = input();
+        int[] x = find();
         write("nums.txt",x);
-
     }
 
 }
+
